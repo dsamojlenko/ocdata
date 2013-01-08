b61d9eaabf281706e218e9c57a4f9ee2bfd55301
Dave Samojlenko <dsamojlenko@gmail.com>
2013-01-07 23:38:07 -0500

    added /api directory to path (for future reasons) and added :agency param

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ app.configure(function () {
     app.use(express.bodyParser());
 });
 
-app.get('/getRoutesForStop/:stop', routes.getRoutesForStop);
-app.get('/getNextTripsForStop/:stop/:route', routes.getNextTripsForStop);
+app.get('/api/:agency/getRoutesForStop/:stop', routes.getRoutesForStop);
+app.get('/api/:agency/getNextTripsForStop/:stop/:route', routes.getNextTripsForStop);
 
 app.listen(3000);
 console.log('Listening on port 3000...');
