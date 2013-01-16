9e7a6c71b478fc25b69abc37e1867e827792c658
Dave Samojlenko <dsamojlenko@gmail.com>
2013-01-15 23:23:56 -0500

    refactored some of the routing code

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require('express'),
-	routes = require('./lib/ocdata.js');
+	ocdata = require('./lib/ocdata.js');
 
 var app = express();
 
@@ -8,8 +8,27 @@ app.configure(function () {
     app.use(express.bodyParser());
 });
 
-app.get('/api/:agency/getRoutesForStop/:stop', routes.getRoutesForStop);
-app.get('/api/:agency/getNextTripsForStop/:stop/:route', routes.getNextTripsForStop);
+app.get('/api/:agency/getRoutesForStop/:stop', function(request, repsonse) {
+	
+	var agency = request.params.agency
+	  , stop = request.params.stop;
+	  
+	ocdata.getRoutesForStop(agency,stop, function(e,data) {
+		response.send( data || { error: "error dude" });
+	});
+});
+
+app.get('/api/:agency/getNextTripsForStop/:stop/:route', function(request, response) {
+	
+	var agency = request.params.agency
+	  , stop = request.params.stop
+	  , route = request.params.route;
+	  
+	ocdata.getNextTripsForStop(agency,stop,route, function(e, data) {
+		response.send( data || { error: "error dude" });
+	});
+	
+});
 
 app.listen(3000);
 console.log('Listening on port 3000...');
