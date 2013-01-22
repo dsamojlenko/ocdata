2a91b02f3be374c860624ce2fedac41930f64c05
Dave Samojlenko <dsamojlenko@gmail.com>
2013-01-21 23:37:16 -0500

    added a function to fix times that are over 24:00hrs

--- models/StopTime.js
+++ models/StopTime.js
@@ -4,10 +4,14 @@ var mongoose = require('mongoose')
 	
 mongoose.model('StopTime', new Schema({
     trip_id           :  { type: String, index: true }
-  , arrival_time      :  { type: String }
-  , departure_time    :  { type: String, index: true }
+  , arrival_time      :  { type: String, index: true, get: utils.fixTime }
+  , departure_time    :  { type: String, index: true, get: utils.fixTime }
   , stop_id           :  { type: String, index: true }
   , stop_sequence     :  { type: Number, index: true }
   , pickup_type       :  { type: String }
   , drop_off_type     :  { type: String }
 }, {collection: 'stoptimes'},{ strict: true }));
+
+
+
+// get: utils.secondsToTime, set: utils.timeToSeconds
